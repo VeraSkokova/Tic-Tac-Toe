e1b769067eabacf9a0747f0294d5e03110335e8f
VeraSkokova <miss.smiley17@gmail.com>
2018-12-02 20:34:06 +0700

    Add navigation menu

---
+++ app/src/main/java/ru/nsu/ccfit/skokova/tic_tac_toe/view/StatisticsView.java
@@ -0,0 +1,4 @@
+package ru.nsu.ccfit.skokova.tic_tac_toe.view;
+
+public interface StatisticsView {
+}
---
+++ app/src/main/java/ru/nsu/ccfit/skokova/tic_tac_toe/view/activity/StatisticsActivity.java
@@ -0,0 +1,14 @@
+package ru.nsu.ccfit.skokova.tic_tac_toe.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.nsu.ccfit.skokova.tic_tac_toe.view.StatisticsView;
+
+public class StatisticsActivity extends AppCompatActivity implements StatisticsView {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
